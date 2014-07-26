fe91b36dab4257591c633df02ddd407553d2e6b0
Ken Mercus Lai <ken.mercus.lai@gmail.com>
2014-07-26 09:28:39 +0800

    personal best: 99 place: 151st

--- Prime Palindrome Golf.py
+++ Prime Palindrome Golf.py
@@ -1,8 +1,6 @@
 def golf(number):
-    while True:
-        number += 1
-        if (str(number) == str(number)[::-1]
-                and all(number % i for i in xrange(2, number))):
-            return number
+    for i in range(number + 1, 99999):
+        if (str(i) == str(i)[::-1] and all(i % j for j in xrange(2, i))):
+            return i
 
 print golf(13)
