d910e84c486867f6ce11d3e571fa8bb759080825
Ken Mercus Lai <ken.mercus.lai@gmail.com>
2014-07-27 02:11:09 +0800

    personal best: 141, place: 56

--- Space Golf.py
+++ Space Golf.py
@@ -1,6 +1,5 @@
 from itertools import permutations as p
 from math import hypot as t
-def golf(h):
-    return round(min([t(*i[0])+sum([t(i[j][0]-i[j+1][0],i[j][1]-i[j+1][1]) for j in range(len(i)-1)]) for i in [i for i in p(h)]]), 2)
+def golf(h):return round(min([t(*i[0])+sum([t(i[j][0]-i[j+1][0],i[j][1]-i[j+1][1]) for j in range(len(i)-1)]) for i in [i for i in p(h)]]),2)
 
 print golf({(2, 2), (2, 8), (8, 8), (5, 5), (8, 2)})
