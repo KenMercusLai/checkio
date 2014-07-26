cddf9ed80f6940185c0a70c18f5f83e15424290d
Ken Mercus Lai <ken.mercus.lai@gmail.com>
2014-07-26 09:21:28 +0800

    personal shortest solution

--- Prime Palindrome Golf.py
+++ Prime Palindrome Golf.py
@@ -1,12 +1,8 @@
-from math import ceil
-
-
 def golf(number):
     while True:
         number += 1
-        index = int(ceil(len(str(number)) / 2.0))
-        if str(number)[:index] == str(number)[-index:][::-1]:
-            if all(number % i for i in xrange(2, number)):
-                return number
+        if (str(number) == str(number)[::-1]
+                and all(number % i for i in xrange(2, number))):
+            return number
 
 print golf(13)
