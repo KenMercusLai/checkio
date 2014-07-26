708c17554c03dabba92abb63da508d32fa60e7a3
Ken Mercus Lai <ken.mercus.lai@gmail.com>
2014-07-27 01:53:45 +0800

    personal best: 80, place 65

--- Space Golf.py
+++ Space Golf.py
@@ -1,14 +1,9 @@
 from itertools import permutations
 from math import hypot
-
-
 def golf(h):
-    distances = []
+    d = []
     for i in [i for i in permutations(h)]:
-        tempDistance = hypot(i[0][0], i[0][1])
-        for j in range(len(i) - 1):
-            tempDistance += hypot(i[j][0] - i[j + 1][0], i[j][1] - i[j + 1][1])
-        distances.append(tempDistance)
-    return round(min(distances), 2)
+        d.append(hypot(i[0][0],i[0][1])+sum([hypot(i[j][0]-i[j+1][0],i[j][1]-i[j+1][1]) for j in range(len(i) - 1)]))
+    return round(min(d), 2)
 
 print golf({(2, 2), (2, 8), (8, 8), (5, 5), (8, 2)})
