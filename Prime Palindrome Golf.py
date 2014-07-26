257423e6d6fa04abfc732ce86fdb46fbd08f36b3
Ken Mercus Lai <ken.mercus.lai@gmail.com>
2014-07-26 09:49:04 +0800

    personal best: 119 place: 119

--- Prime Palindrome Golf.py
+++ Prime Palindrome Golf.py
@@ -1,5 +1,5 @@
 def golf(number):
-    return [i for i in range(number + 1, 99999) if str(i) == str(i)[::-1] and all(i % j for j in xrange(2, i))][0]
+    return [i for i in range(number + 1, 99999) if i == int(str(i)[::-1]) and all(i % j for j in range(2, i))][0]
 
 
 print golf(90000)
