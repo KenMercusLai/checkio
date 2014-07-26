cbf4a01d492fdbe3a7103838c18dca05e080984d
Ken Mercus Lai <ken.mercus.lai@gmail.com>
2014-07-26 16:44:06 +0800

    Fizz Buzz

---
+++ Fizz Buzz.py
@@ -0,0 +1,24 @@
+# Your optional code here
+# You can import some modules or create additional functions
+
+
+def checkio(number):
+    result = []
+    if number % 3 == 0:
+        result.append('Fizz')
+    if number % 5 == 0:
+        result.append('Buzz')
+    if result:
+        return ' '.join(result)
+    return str(number)
+
+# Some hints:
+# Convert a number in the string with str(n)
+
+# These "asserts" using only for self-checking and not necessary for
+# auto-testing
+if __name__ == '__main__':
+    assert checkio(15) == "Fizz Buzz", "15 is divisible by 3 and 5"
+    assert checkio(6) == "Fizz", "6 is divisible by 3"
+    assert checkio(5) == "Buzz", "5 is divisible by 5"
+    assert checkio(7) == "7", "7 is not divisible by 3 or 5"
