a2465424ab37d4907979bd6e5a13c028cfabfe54
Ken Mercus Lai <ken.mercus.lai@gmail.com>
2014-07-30 19:33:25 +0800

    question Pawn Brotherhood add

---
+++ Pawn Brotherhood.py
@@ -0,0 +1,7 @@
+def safe_pawns(pawns):
+    return 0
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert safe_pawns({"b4", "d4", "f4", "c3", "e3", "g5", "d2"}) == 6
+    assert safe_pawns({"b4", "c4", "d4", "e4", "f4", "g4", "e5"}) == 1
