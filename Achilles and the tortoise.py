c88b4e7b20fa898b65b7535fabc26326e07e463b
Ken Mercus Lai <ken.mercus.lai@gmail.com>
2014-07-30 19:28:49 +0800

    Achilles and the tortoise solved

--- Achilles and the tortoise.py
+++ Achilles and the tortoise.py
@@ -1,12 +1,14 @@
 def chase(a1_speed, t2_speed, advantage):
-    return 0
+    advantageDistance = t2_speed * advantage
+    return round(advantage + advantageDistance * 1.0 / (a1_speed - t2_speed),
+                 8)
 
 if __name__ == '__main__':
-    #These "asserts" using only for self-checking and not necessary for auto-testing
+    # These "asserts" using only for self-checking and not necessary for
+    # auto-testing
     def almost_equal(checked, correct, significant_digits):
         precision = 0.1 ** significant_digits
         return correct - precision < checked < correct + precision
 
     assert almost_equal(chase(6, 3, 2), 4, 8), "example"
     assert almost_equal(chase(10, 1, 10), 11.111111111, 8), "long number"
-
