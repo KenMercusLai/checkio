423587820d33ae7a42adadb312b0f22c6c53b74b
Ken Mercus Lai <ken.mercus.lai@gmail.com>
2014-08-11 22:55:41 +0800

    super root done

---
+++ Super Root.py
@@ -0,0 +1,26 @@
+def super_root(number):
+    x = 0
+    base = 1.0
+    while not number - 0.001 < x ** x < number + 0.001:
+        if x ** x <= number - 0.001:
+            x += base
+        else:
+            base = base / 2
+            x -= base
+    return x
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for
+    # auto-testing
+    def check_result(function, number):
+        result = function(number)
+        if not isinstance(result, (int, float)):
+            print("The result should be a float or an integer.")
+            return False
+        p = result ** result
+        if number - 0.001 < p < number + 0.001:
+            return True
+        return False
+    assert check_result(super_root, 4), "Square"
+    assert check_result(super_root, 9), "Cube"
+    assert check_result(super_root, 81), "Eighty one"
