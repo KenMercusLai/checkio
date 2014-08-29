c8165444e2d8f2f2d6fc0c2817bb5a75a23d2a6b
Ken Mercus Lai <ken.mercus.lai@gmail.com>
2014-08-30 00:33:30 +0800

    text holes golf personal best: 148

--- Text Holes Golf.py
+++ Text Holes Golf.py
@@ -1,63 +1,71 @@
 def golf(t):
-    c=0
-    for r in range(1,len(t)-1):
-        for i in range(1,len(t[r])-1):
-            try:
-                if t[r][i]==' 'and' 'not in[t[r][i-1],t[r][i+1],t[r-1][i],t[r+1][i],t[r+1][i+1],t[r-1][i-1],t[r+1][i-1],t[r-1][i+1]]:
-                    c+=1
-            except:pass
-    return c
+ c=0
+ for j in range(0,len(t)-2):
+  for i in range(0,min(map(len,t[j:j+3]))-1):
+   x,y,z=t[j:j+3];u=x[i:i+3]+y[i:i+3]+z[i:i+3]
+   if all([y[i+1]==' ',u.count(' ')*9==len(u)]):c+=1
+ return c
 
 print golf([
-    "How are you doing?",
-    "I'm fine. OK.",
-    "Lorem Ipsum?",
-    "Of course!!!",
-    "1234567890",
-    "0        0",
-    "1234567890",
-    "Fine! good buy!"])
+ "How are you doing?",
+ "I'm fine. OK.",
+ "Lorem Ipsum?",
+ "Of course!!!",
+ "1234567890",
+ "0        0",
+ "1234567890",
+ "Fine! good buy!"])
 
 print golf([
-    "Lorem ipsum dolor",
-    "sit amet,",
-    "consectetuer",
-    "adipiscing elit.",
-    "Aenean commodo",
-    "ligula eget dolor.",
-    "Aenean massa. Cum",
-    "sociis natoque",
-    "penatibus et magnis",
-    "dis parturient",
-    "montes, nascetur",
-    "ridiculus mus. Donec",
-    "quam felis,",
-    "ultricies nec,",
-    "pellentesque eu,",
-    "pretium quis, sem.",
-    "Nulla consequat",
-    "massa quis enim.",
-    "Donec pede justo,",
-    "fringilla vel,"])
+ "Lorem ipsum dolor",
+ "sit amet,",
+ "consectetuer",
+ "adipiscing elit.",
+ "Aenean commodo",
+ "ligula eget dolor.",
+ "Aenean massa. Cum",
+ "sociis natoque",
+ "penatibus et magnis",
+ "dis parturient",
+ "montes, nascetur",
+ "ridiculus mus. Donec",
+ "quam felis,",
+ "ultricies nec,",
+ "pellentesque eu,",
+ "pretium quis, sem.",
+ "Nulla consequat",
+ "massa quis enim.",
+ "Donec pede justo,",
+ "fringilla vel,"])
 
 print golf([
-    "Lorem ipsum dolor",
-    "sit amet,",
-    "consectetuer",
-    "adipiscing elit.",
-    "Aenean commodo",
-    "ligula eget dolor.",
-    "Aenean massa. Cum",
-    "sociis natoque",
-    "penatibus et magnis",
-    "dis parturient",
-    "montes, nascetur",
-    "ridiculus mus. Donec",
-    "quam felis,",
-    "ultricies nec,",
-    "pellentesque eu,",
-    "pretium quis, sem.",
-    "Nulla consequat",
-    "massa quis enim.",
-    "Donec pede justo,",
-    "fringilla vel,"])
+ "Lorem ipsum dolor",
+ "sit amet,",
+ "consectetuer",
+ "adipiscing elit.",
+ "Aenean commodo",
+ "ligula eget dolor.",
+ "Aenean massa. Cum",
+ "sociis natoque",
+ "penatibus et magnis",
+ "dis parturient",
+ "montes, nascetur",
+ "ridiculus mus. Donec",
+ "quam felis,",
+ "ultricies nec,",
+ "pellentesque eu,",
+ "pretium quis, sem.",
+ "Nulla consequat",
+ "massa quis enim.",
+ "Donec pede justo,",
+ "fringilla vel,"])
+
+print golf([
+ "How are you doing?",
+ "I'm fine. OK.",
+ "Lorem Ipsum?",
+ "Of course!!!",
+ "1234567890",
+ "0        0",
+ "1234567890",
+ "Fine! good buy!"])
