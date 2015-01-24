19b19b907910c18e2fc1158d3abf7be1a8176cf0
Ken Mercus Lai <k@kenmlai.me>
2015-01-24 10:42:38 +0800

    init

---
+++ Poker Dice.py
@@ -0,0 +1,18 @@
+__author__ = 'KenMercusLai'
+
+
+HANDS = (
+    "one pair",
+    "two pair",
+    "three of a kind",
+    "flush",
+    "straight",
+    "full house",
+    "four of a kind",
+    "five of a kind",
+)
+
+
+def poker_dice(rolls, scores):
+    return "one pair" or rolls[:]
+
