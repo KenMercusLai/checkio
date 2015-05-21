1c4409f79c8e5756f0417a93590f73c466c513d3
Ken Mercus Lai <k@kenmlai.me>
2015-05-21 20:48:30 +0800

    init code

---
+++ univocalic_factaral.py
@@ -0,0 +1,12 @@
+def a_factaral(n):
+    if n == 0:
+        return 1
+    return n * a_factaral(n - 1)
+
+# this assertion should be stripped after self-testing.
+if __name__ == '__main__':
+    assert a_factaral(0) == 1, "Zero"
+    assert a_factaral(1) == 1, "One"
+    assert a_factaral(2) == 2, "Two"
+    assert a_factaral(3) == 6, "Six"
+
