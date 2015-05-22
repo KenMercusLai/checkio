13bff502cb644a89d97b48a73f0cedd1ef18b51e
Ken Mercus Lai <k@kenmlai.me>
2015-05-22 08:38:20 +0800

    still contains e

--- univocalic_factaral.py
+++ univocalic_factaral.py
@@ -1,7 +1,9 @@
-def a_factaral(n):
-    if n == 0:
-        return 1
-    return n * a_factaral(n - 1)
+# def a_factaral(n):
+#     if n == 0:
+#         return 1
+#     return n * a_factaral(n - 1)
+
+a_factaral = lambda n: 1 if n == 1 else n * a_factaral(n - 1)
 
 # this assertion should be stripped after self-testing.
 if __name__ == '__main__':
@@ -9,4 +11,3 @@ if __name__ == '__main__':
     assert a_factaral(1) == 1, "One"
     assert a_factaral(2) == 2, "Two"
     assert a_factaral(3) == 6, "Six"
-
