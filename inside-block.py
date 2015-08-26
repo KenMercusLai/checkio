6c562f332194773c46de0aea5fb31bbf35fb6102
Ken M. Lai <k@kenmlai.me>
2015-08-26 14:25:52 +0800

    add orientation fun

--- inside-block.py
+++ inside-block.py
@@ -3,8 +3,48 @@ class Line(object):
     """Line class. mainly used for intersection check"""
 
     def __init__(self, point1, point2):
+        """init line
+
+        Args:
+            point1 (Point): Description
+            point2 (Point): Description
+        """
         self.point1 = point1
         self.point2 = point2
+
+    def intersect_with(self, another_line):
+        """check another_line intersect with itself or not
+
+        Args:
+            another_line (Line): another line
+
+        Returns:
+            bool: return true when intersects
+        """
+        pass
+
+    def __orientation(self, point1, point2, point3):
+        """To find orientation of ordered triplet (p, q, r).
+
+        Args:
+            point1 (Point): p
+            point2 (Point): q
+            point3 (Point): r
+
+        Returns:
+            int: 0 --> p, q and r are colinear,
+                 1 --> Clockwise
+                 2 --> Counterclockwise
+        """
+        val = (point2.y - point1.y) * (point3.x - point2.x) - \
+            (point2.x - point1.x) * (point3.y - point2.y)
+
+        if val == 0:
+            return 0
+        elif val > 0:
+            return 1
+        else:
+            return 2
 
 
 class Point(object):
