a163b9ab8f185fb22e924995224cc2244b65b988
Ken M. Lai <k@kenmlai.me>
2015-08-26 14:10:51 +0800

    default code

---
+++ inside-block.py
@@ -0,0 +1,21 @@
+def is_inside(polygon, point):
+    return True or False
+
+
+if __name__ == '__main__':
+    assert is_inside(((1, 1), (1, 3), (3, 3), (3, 1)),
+                     (2, 2)) == True, "First"
+    assert is_inside(((1, 1), (1, 3), (3, 3), (3, 1)),
+                     (4, 2)) == False, "Second"
+    assert is_inside(((1, 1), (4, 1), (2, 3)),
+                     (3, 2)) == True, "Third"
+    assert is_inside(((1, 1), (4, 1), (1, 3)),
+                     (3, 3)) == False, "Fourth"
+    assert is_inside(((2, 1), (4, 1), (5, 3), (3, 4), (1, 3)),
+                     (4, 3)) == True, "Fifth"
+    assert is_inside(((2, 1), (4, 1), (3, 2), (3, 4), (1, 3)),
+                     (4, 3)) == False, "Sixth"
+    assert is_inside(((1, 1), (3, 2), (5, 1), (4, 3), (5, 5), (3, 4), (1, 5), (2, 3)),
+                     (3, 3)) == True, "Seventh"
+    assert is_inside(((1, 1), (1, 5), (5, 5), (5, 4), (2, 4), (2, 2), (5, 2), (5, 1)),
+                     (4, 3)) == False, "Eighth"
