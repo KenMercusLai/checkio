449d5caeeec0de6f585ce7fa59279a8e34f186be
Ken M. Lai <k@kenmlai.me>
2015-08-26 14:15:01 +0800

    basic classes for Point and Line

--- inside-block.py
+++ inside-block.py
@@ -1,3 +1,21 @@
+class Line(object):
+
+    """Line class. mainly used for intersection check"""
+
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+
+class Point(object):
+
+    """class for point, mainly used in Line class"""
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
 def is_inside(polygon, point):
     return True or False
 
