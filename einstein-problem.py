cb15c45ff64de67d4edfa40d13af04ad9e3d2caf
Ken M. Lai <k@kenmlai.me>
2015-08-31 17:22:55 +0800

    init code

---
+++ einstein-problem.py
@@ -0,0 +1,38 @@
+COLORS = ['blue', 'green', 'red', 'white', 'yellow']
+PETS = ['cat', 'bird', 'dog', 'fish', 'horse']
+BEVERAGES = ['beer', 'coffee', 'milk', 'tea', 'water']
+CIGARETTES = ['Rothmans', 'Dunhill', 'Pall Mall', 'Winfield', 'Marlboro']
+NATIONALITY = ['Brit', 'Dane', 'German', 'Norwegian', 'Swede']
+NUMBERS = ['1', '2', '3', '4', '5']
+QUESTIONS = ["number", "color", "nationality", "beverage", "cigarettes", "pet"]
+
+
+def answer(relations, question):
+    return "Answer"
+
+
+if __name__ == '__main__':
+    assert answer(('Norwegian-Dunhill', 'Marlboro-blue', 'Brit-3',
+                   'German-coffee', 'beer-white', 'cat-water',
+                   'horse-2', 'milk-3', '4-Rothmans',
+                   'dog-Swede', 'Norwegian-1', 'horse-Marlboro',
+                   'bird-Brit', '4-green', 'Winfield-beer',
+                   'Dane-blue', '5-dog', 'blue-horse',
+                   'yellow-cat', 'Winfield-Swede', 'tea-Marlboro'),
+                  'fish-color') == 'green'  # What is the color of the house where the Fish lives?
+    assert answer(('Norwegian-Dunhill', 'Marlboro-blue', 'Brit-3',
+                   'German-coffee', 'beer-white', 'cat-water',
+                   'horse-2', 'milk-3', '4-Rothmans',
+                   'dog-Swede', 'Norwegian-1', 'horse-Marlboro',
+                   'bird-Brit', '4-green', 'Winfield-beer',
+                   'Dane-blue', '5-dog', 'blue-horse',
+                   'yellow-cat', 'Winfield-Swede', 'tea-Marlboro'),
+                  'tea-number') == '2'  # What is the number of the house where tea is favorite beverage?
+    assert answer(('Norwegian-Dunhill', 'Marlboro-blue', 'Brit-3',
+                   'German-coffee', 'beer-white', 'cat-water',
+                   'horse-2', 'milk-3', '4-Rothmans',
+                   'dog-Swede', 'Norwegian-1', 'horse-Marlboro',
+                   'bird-Brit', '4-green', 'Winfield-beer',
+                   'Dane-blue', '5-dog', 'blue-horse',
+                   'yellow-cat', 'Winfield-Swede', 'tea-Marlboro'),
+                  'Norwegian-beverage') == 'water'  # What is the favorite beverage of the Norwegian man?
