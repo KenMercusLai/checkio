ae6812a014d51750febef0f59d6fa96f7a630c7d
Ken M. Lai <k@kenmlai.me>
2015-09-07 21:18:59 +0800

    workable code, can guarantee to win

--- x_and_o.py
+++ x_and_o.py
@@ -1,13 +1,48 @@
+from copy import deepcopy
+unitlists = [[(i, j) for j in range(3)] for i in range(3)] + \
+    [[(j, i) for j in range(3)] for i in range(3)] + \
+    [[(j, j) for j in range(3)]] + \
+    [[(j, 2 - j) for j in range(3)]]
+
+
+def remove_units(grid, mark_to_remove):
+    grid = list(grid)
+    result = deepcopy(unitlists)
+    for i in range(3):
+        for j in range(3):
+            if grid[i][j] == mark_to_remove:
+                result = [k for k in result if (i, j) not in k]
+    return result
+
+def most_valuable_cell(unitlists):
+    result = {}
+    for i in unitlists:
+        for j in i:
+            if j not in result:
+                result[j] = 0
+            result[j] += 1
+    return sorted(result.items(), key=lambda x: x[1], reverse=True)
+
 def x_and_o(grid, your_mark):
+    if your_mark == 'X':
+        remained_units = remove_units(grid, 'O')
+    else:
+        remained_units = remove_units(grid, 'X')
+    cell_list = most_valuable_cell(remained_units)
+    for i in cell_list:
+        if list(grid)[i[0][0]][i[0][1]] == '.':
+            return i[0]
     return 1, 1
 
 
 if __name__ == '__main__':
-    #These "asserts" using only for self-checking and not necessary for auto-testing
+    # These "asserts" using only for self-checking and not necessary for
+    # auto-testing
     from random import choice
 
     def random_bot(grid, mark):
-        empties = [(x, y) for x in range(3) for y in range(3) if grid[x][y] == "."]
+        empties = [(x, y) for x in range(3)
+                   for y in range(3) if grid[x][y] == "."]
         return choice(empties) if empties else (None, None)
 
     def referee(field):
@@ -28,10 +63,12 @@ if __name__ == '__main__':
             x, y = bot_algorithm(grid, bot_mark)
             grid[x][y] = "X"
         while True:
-            user_result = user_func(tuple("".join(row) for row in grid), user_mark)
+            user_result = user_func(tuple("".join(row)
+                                          for row in grid), user_mark)
             if (not isinstance(user_result, (tuple, list)) or len(user_result) != 2 or
                     not all(isinstance(u, int) and 0 <= u < 3 for u in user_result)):
-                print("The result must be a list/tuple of two integers from 0 to 2.")
+                print(
+                    "The result must be a list/tuple of two integers from 0 to 2.")
                 return False
 
             if grid[user_result[0]][user_result[1]] != ".":
