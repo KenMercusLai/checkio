888f137ab396d0d14c39721bcc855549422d20a9
Ken M. Lai <k@kenmlai.me>
2015-09-07 17:13:56 +0800

    test passed

--- keywords_finder.py
+++ keywords_finder.py
@@ -1,4 +1,60 @@
+def get_matched_positions(text, word_list):
+    """get all matched positions
+
+    Args:
+        text (str): Description
+        word_list (list): a list of keywords in lowercase
+
+    Returns:
+        list: list of tuples for start and end position contain matched words
+    """
+    results = []
+    for i in word_list:
+        start_position = 0
+        word_length = len(i)
+        matched = text.find(i, 0)
+        while matched != -1:
+            results.append((matched, matched + word_length - 1))
+            start_position += matched + word_length
+            matched = text.find(i, start_position)
+    return results
+
+
+def is_contiguious(pair1, pair2):
+    if pair1[0] > pair2[1] or pair2[0] > pair1[1]:
+        return False
+    return True
+
+
+def merge_contiguities(matched_pairs):
+    changed = True
+    while changed:
+        changed = False
+        for i in range(len(matched_pairs)):
+            for j in range(i + 1, len(matched_pairs)):
+                pair1, pair2 = matched_pairs[i], matched_pairs[j]
+                if is_contiguious(pair1, pair2):
+                    new_pair = (
+                        min(pair1[0], pair2[0]), max(pair1[1], pair2[1]))
+                    matched_pairs.remove(pair1)
+                    matched_pairs.remove(pair2)
+                    matched_pairs.append(new_pair)
+                    changed = True
+                if changed:
+                    break
+            if changed:
+                break
+    return matched_pairs
+
+
 def checkio(text, words):
+    matched_pairs = get_matched_positions(
+        text.lower(), [i.lower() for i in words.split()])
+    for i in sorted(merge_contiguities(matched_pairs), reverse=True):
+        temp_text = list(text)
+        temp_text.insert(i[1] + 1, '</span>')
+        temp_text.insert(i[0], '<span>')
+        text = ''.join(temp_text)
     return text
 
 # These "asserts" using only for self-checking and not necessary for
