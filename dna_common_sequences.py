c06c3eee398199f3bf35b93916245401180621e5
Ken M. Lai <k@kenmlai.me>
2015-09-08 21:38:15 +0800

    new algorithm: 1. start from minimal matches(2 chars) 2. build matched indexes 3. expand one more char based on step.1 4. loop unitl no longer matched sequence

--- dna_common_sequences.py
+++ dna_common_sequences.py
@@ -1,3 +1,5 @@
+from itertools import combinations
+from copy import deepcopy
 CACHE = {}
 
 
@@ -19,15 +21,49 @@ def generate_subsequences(sequence, length):
     return result
 
 
-def common(first, second):
-    max_common_length = min(len(first), len(second))
-    for sequence_length in range(max_common_length, 1, -1):
-        first_set = set(generate_subsequences(first, sequence_length))
-        second_set = set(generate_subsequences(second, sequence_length))
-        intersection = first_set.intersection(second_set)
-        if intersection:
-            return ','.join(sorted(intersection))
-    return ""
+def common(first, second):    
+    # build common str matches at length 2
+    result = {}
+    for i in combinations(range(len(first)), 2):
+        first_subsequence = [first[ii] for ii in i]
+        for j in combinations(range(len(second)), 2):
+            second_subsequence = [second[jj] for jj in j]
+            if first_subsequence == second_subsequence:
+                if i not in result:
+                    result[i] = []
+                result[i].append(j)
+
+    longer_result = 1
+    while longer_result:
+        print(len(result))
+        longer_result = {}
+        for i in result:
+            i = list(i)
+            # remove unexpanable subsequences early
+            if i[-1] == len(first) - 1:
+                continue
+            for j in result[tuple(i)]:
+                j = list(j)
+                if j[-1] == len(second) - 1:
+                    continue
+
+                # test combinations
+                for expand_i in range(i[-1]+1, len(first)):
+                    first_subsequence = [first[ii] for ii in i + [expand_i]]
+                    for expand_j in range(j[-1]+1, len(second)):
+                        second_subsequence = [second[jj] for jj in j + [expand_j]]
+                        if first_subsequence == second_subsequence:
+                            if tuple(i + [expand_i]) not in longer_result:
+                                longer_result[tuple(i + [expand_i])] = []
+                            longer_result[tuple(i + [expand_i])].append(tuple(j + [expand_j]))
+        if longer_result:
+            result = deepcopy(longer_result)
+    print(result)
+    final_result = []
+    for i in result:
+        final_result.append(''.join([first[j] for j in i]))
+    return ','.join(sorted(final_result))
+    
 
 if __name__ == '__main__':
     # These "asserts" using only for self-checking and not necessary for
@@ -35,5 +71,5 @@ if __name__ == '__main__':
     assert common("ACGTC", "TTACTC") == "ACTC", "One"
     assert common("CGCTA", "TACCG") == "CC,CG,TA", "Two"
     assert common("GCTT", "AAAAA") == "", "None"
-    print(generate_subsequences(
-        'AACGTTTTGGGTTTAGAGAAAGTGCTCACAGTAGGTACGTCCCCCAGACCCCACGCCAATGTAT', 42))
+    assert common('TTGGTGTCGCTAGACC', 'CGCTAGTGGGGAAT') == 'TTGGGGAA'
+    common('AACGTTTTGGGTTTAGAGAAAGTGCTCACAGTAGGTACGTCCCCCAGACCCCACGCCAATGTAT', 'TTTGGGAATGCAATTTAGCTCACAGAGCATACAATGAGAACCACCGAGATCATATTAAGTCTCC') == 'TTTGGGAAGAAAGCTCACAGAGTACGAGACCCCAGCAATGTT,TTTGGGAAGAAAGCTCACAGAGTACGAGACCCCAGCAATTAT,TTTGGGAAGAAAGCTCACAGAGTACTAGACCCCAGCAATGTT,TTTGGGAAGAAAGCTCACAGAGTACTAGACCCCAGCAATTAT,TTTGGGAAGAATGCTCACAGAGTACGAGACCCCAGCAATGTT,TTTGGGAAGAATGCTCACAGAGTACGAGACCCCAGCAATTAT,TTTGGGAAGAATGCTCACAGAGTACTAGACCCCAGCAATGTT,TTTGGGAAGAATGCTCACAGAGTACTAGACCCCAGCAATTAT'
