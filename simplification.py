6f54b3ae899d4e62bb0a0e992ef30973bac8defe
Ken M. Lai <k@kenmlai.me>
2015-10-11 11:08:43 +0800

    class Polynomial works

--- simplification.py
+++ simplification.py
@@ -1,8 +1,101 @@
-def simplify(expr):
-    return expr
+import re
+import functools
+import operator
 
-if __name__ == "__main__": # pragma: no cover
-    #These "asserts" using only for self-checking and not necessary for auto-testing
+
+class Polynomial:
+
+    def __init__(self, val):
+        if isinstance(val, list):
+            self.plist = val
+
+    def __add__(self, other):
+        if len(self.plist) > len(other.plist):
+            new = [i for i in self.plist]
+            for i in range(len(other.plist)):
+                new[i] += other.plist[i]
+        else:
+            new = [i for i in other.plist]
+            for i in range(len(self.plist)):
+                new[i] += self.plist[i]
+        return Polynomial(new)
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __sub__(self, other):
+        return self.__add__(other.__neg__())
+
+    def __rsub__(self, other):
+        return -Polynomial.__sub__(other)
+
+    def __mul__(self, other):
+        result = []
+        for i in range(len(other.plist)):
+            result.append(
+                Polynomial([0] * i + [j * other.plist[i] for j in self.plist]))
+        return functools.reduce(operator.add, result)
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __neg__(self):
+        return Polynomial([-1]) * self
+
+    def __repr__(self):
+        result = []
+        for i in range(len(self.plist)):
+            if i == 0:
+                result.insert(0, str(self.plist[i]))
+            else:
+                if self.plist[i] == 0:
+                    continue
+                else:
+                    if self.plist[i] == 1:
+                        base_str = 'x'
+                    else:
+                        base_str = str(self.plist[i]) + '*x'
+                    if i == 1:
+                        result.insert(0, base_str)
+                    else:
+                        result.insert(0, base_str + '**' + str(i))
+        for i in range(1, len(result)):
+            if result[i][0] != '-':
+                result[i] = '+' + result[i]
+
+        if result[-1] == '+0' and len(result) > 1:
+            result.pop()
+        return ''.join(result)
+
+
+def tokenise(expr):
+    symbols = list(expr)
+    for i in range(len(symbols)):
+        try:
+            int(symbols[i])
+            symbols[i] = Polynomial([int(symbols[i])])
+        except ValueError:
+            if symbols[i] == 'x':
+                symbols[i] = Polynomial([0, 1])
+
+    while '-' in symbols:
+        index = symbols.index('-')
+        if index == 0 or symbols[index - 1] == '(':
+            symbols = symbols[:index] + \
+                [-symbols[index + 1]] + symbols[index + 2:]
+        else:
+            symbols = symbols[
+                :index] + ['+', -symbols[index + 1].__neg__()] + symbols[index + 2:]
+    return symbols
+
+
+def simplify(expr):
+    print(tokenise(expr))
+
+
+if __name__ == "__main__":  # pragma: no cover
+    # These "asserts" using only for self-checking and not necessary for
+    # auto-testing
     assert simplify("(x-1)*(x+1)") == "x**2-1", "First and simple"
     assert simplify("(x+1)*(x+1)") == "x**2+2*x+1", "Almost the same"
     assert simplify("(x+3)*x*2-x*x") == "x**2+6*x", "Different operations"
@@ -11,4 +104,3 @@ if __name__ == "__main__": # pragma: no cover
     assert simplify("x*x-(x-1)*(x+1)-1") == "0", "Zero"
     assert simplify("5-5-x") == "-x", "Negative C1"
     assert simplify("x*x*x-x*x*x-1") == "-1", "Negative C0"
-
--- tests/test_simplification.py
+++ tests/test_simplification.py
@@ -1,8 +1,21 @@
 import unittest
-from ..simplification import simplify
+from ..simplification import simplify, Polynomial
 
 
 class Tests(unittest.TestCase):
+
+    def test_Polynomial(self):
+        assert str(Polynomial([1]) + Polynomial([1])) == '2'
+        assert str(Polynomial([3]) + Polynomial([0, 4]) +
+                   Polynomial([-24]) + Polynomial([0, -2])) == '2*x-21'
+        assert str(Polynomial([-1, 1]) * Polynomial([-1, 1])) == 'x**2-2*x+1'
+        assert str(-Polynomial([-1, 1])) == '-1*x+1'
+        assert str(-Polynomial([1])) == '-1'
+        assert str(Polynomial([-2, 2]) * Polynomial([2, 2])) == '4*x**2-4'
+        assert str(Polynomial([-2, 1]) * Polynomial([2, 1])
+                   * Polynomial([2, 1])) == 'x**3+2*x**2-4*x-8'
+        assert str(Polynomial([2]) * Polynomial([3, 2]) -
+                   Polynomial([0, 1]) + Polynomial([0, 0, 0, 0, 1])) == 'x**4+3*x+6'
 
     def test_Extra(self):
         assert simplify('(x-1)*(x-1)') == 'x**2-2*x+1'
@@ -17,15 +30,23 @@ class Tests(unittest.TestCase):
         assert simplify('(x-2)*(x+2)*(x+2)') == 'x**3+2*x**2-4*x-8'
 
     def test_Generated(self):
-        assert simplify('84-x+x*37*80+78-98+x-(x)+(23)*x+x-x+x+78-(x*32+61+x-10*x*x)-(59*x)-(x)-x*49+x+23*72+0+(x+x)+(x*(x))') == '11*x**2+2844*x+1737'
-        assert simplify('74-x-15+x+31*x+95*(x+88)*38-x-25-23+x*x+49+x-(47+x+28-43*x-x-39+x-x-83*62)') == 'x**2+3684*x+322850'
-        assert simplify('(x+13-(x*x*x)*x-(x)-x+19-x*10*(x*62)+x-x*0*(66)*83-93+x-(21*x+(85-77+x-x)))') == '-x**4-620*x**2-20*x-69'
-        assert simplify('79+15*7-x+x-x+48-65+x-38+x+46*x*x*(3)+0+(45)') == '138*x**2+x+174'
-        assert simplify('98*85*x*(x-x-(x*(x-41-(x)+3-31+(41)*54*13-x+(x))))') == '-239179290*x**2'
-        assert simplify('24*45*(23*42+73-x)-x-75-13-41*(47-49*(x*25*(x*(x-(x-48*16+55*97)+x-54))))') == '50225*x**3-232089725*x**2-1081*x+1120105'
-        assert simplify('x+x-x+x*x+x*x*x-x*x*x*(4*x+84*x*x-x*x*x-x-x)+x-x-x') == 'x**6-84*x**5-2*x**4+x**3+x**2'
+        assert simplify(
+            '84-x+x*37*80+78-98+x-(x)+(23)*x+x-x+x+78-(x*32+61+x-10*x*x)-(59*x)-(x)-x*49+x+23*72+0+(x+x)+(x*(x))') == '11*x**2+2844*x+1737'
+        assert simplify(
+            '74-x-15+x+31*x+95*(x+88)*38-x-25-23+x*x+49+x-(47+x+28-43*x-x-39+x-x-83*62)') == 'x**2+3684*x+322850'
+        assert simplify(
+            '(x+13-(x*x*x)*x-(x)-x+19-x*10*(x*62)+x-x*0*(66)*83-93+x-(21*x+(85-77+x-x)))') == '-x**4-620*x**2-20*x-69'
+        assert simplify(
+            '79+15*7-x+x-x+48-65+x-38+x+46*x*x*(3)+0+(45)') == '138*x**2+x+174'
+        assert simplify(
+            '98*85*x*(x-x-(x*(x-41-(x)+3-31+(41)*54*13-x+(x))))') == '-239179290*x**2'
+        assert simplify(
+            '24*45*(23*42+73-x)-x-75-13-41*(47-49*(x*25*(x*(x-(x-48*16+55*97)+x-54))))') == '50225*x**3-232089725*x**2-1081*x+1120105'
+        assert simplify(
+            'x+x-x+x*x+x*x*x-x*x*x*(4*x+84*x*x-x*x*x-x-x)+x-x-x') == 'x**6-84*x**5-2*x**4+x**3+x**2'
         assert simplify('72*x*x-(x+x-x*x)+x*81') == '73*x**2+79*x'
-        assert simplify('11+(x)*(x*x)*x*(x+x*20-(15)*x-(40+(x)-(x*x*x+x+x)*x+47-(x)))') == 'x**8+2*x**6+6*x**5-87*x**4+11'
+        assert simplify(
+            '11+(x)*(x*x)*x*(x+x*20-(15)*x-(40+(x)-(x*x*x+x+x)*x+47-(x)))') == 'x**8+2*x**6+6*x**5-87*x**4+11'
 
     def test_Basics(self):
         assert simplify('(x-1)*(x+1)') == 'x**2-1'
