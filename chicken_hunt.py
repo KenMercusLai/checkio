82985c41768d5c72f6469c7bca6726341ac56bb8
Ken M. Lai <k@kenmlai.me>
2015-10-13 15:19:55 +0800

    WIP

--- chicken_hunt.py
+++ chicken_hunt.py
@@ -1,3 +1,6 @@
+from copy import deepcopy
+import operator
+
 DIRS = {
     "N": (-1, 0),
     "S": (1, 0),
@@ -11,20 +14,124 @@ DIRS = {
 }
 
 
+def translate_move(original, new_position):
+    if original == new_position:
+        return ''
+    if original[0] > new_position[0]:
+        if original[1] > new_position[1]:
+            return 'NW'
+        elif original[1] == new_position[1]:
+            return 'N'
+        else:
+            return 'NE'
+    elif original[0] == new_position[0]:
+        if original[1] > new_position[1]:
+            return 'W'
+        else:
+            return 'E'
+    else:
+        if original[1] > new_position[1]:
+            return 'SW'
+        elif original[1] == new_position[1]:
+            return 'S'
+        else:
+            return 'SE'
+
+
+def position_of(character, yard):
+    index = ''.join(yard).index(character)
+    return index // len(yard[0]), index % len(yard[0])
+
+
+def possible_move_of(y, x, yard):
+    rows, cols = len(yard), len(yard[0])
+    return [(y + i[0], x + i[1])
+            for i in DIRS.values()
+            if 0 <= y + i[0] <= rows - 1
+            and 0 <= x + i[1] <= cols - 1
+            and yard[y + i[0]][x + i[1]] not in ['X', 'I', 'S']]
+
+
+def get_adjencies(y, x, yard):
+    result = set()
+    open_position = set()
+    closed_position = set()
+    I = position_of('I', yard)
+    for i in possible_move_of(I[0], I[1], yard):
+        closed_position.add(i)
+    S = position_of('S', yard)
+    for i in possible_move_of(S[0], S[1], yard):
+        closed_position.add(i)
+    if (y, x) not in closed_position:
+        open_position.add((y, x))
+    while open_position:
+        next_position = open_position.pop()
+        result.add(next_position)
+        closed_position.add(next_position)
+        for i in possible_move_of(next_position[0], next_position[1], yard):
+            if i not in closed_position:
+                open_position.add(i)
+    return result
+
+
+def chicken_space(yard):
+    C = position_of('C', yard)
+    adjencies = get_adjencies(C[0], C[1], yard)
+    I = position_of('I', yard)
+    for i in possible_move_of(I[0], I[1], yard):
+        try:
+            adjencies.remove(i)
+        except KeyError:
+            pass
+    S = position_of('S', yard)
+    for i in possible_move_of(S[0], S[1], yard):
+        try:
+            adjencies.remove(i)
+        except KeyError:
+            pass
+    return adjencies
+
+
 def hunt(yard):
-    return "N"
+    original_space = chicken_space(yard)
+    for i in yard:
+        print(i)
+
+    I = position_of('I', yard)
+    result = {}
+    for i in possible_move_of(I[0], I[1], yard):
+        temp_yard = deepcopy(yard)
+        temp_yard = list(temp_yard)
+        temp_yard[I[0]] = list(temp_yard[I[0]])
+        temp_yard[I[0]][I[1]] = temp_yard[i[0]][i[1]]
+        temp_yard[I[0]] = ''.join(temp_yard[I[0]])
+        temp_yard[i[0]] = list(temp_yard[i[0]])
+        temp_yard[i[0]][i[1]] = 'I'
+        temp_yard[i[0]] = ''.join(temp_yard[i[0]])
+        temp_result = chicken_space(temp_yard)
+        if len(temp_result) <= len(original_space):
+            result[i] = chicken_space(temp_yard)
+    if not result:
+        return ''
+    print(result)
+
+    C = position_of('C', yard)
+    if C in result:
+        return translate_move(I, C)
+    return translate_move(I, sorted(result.items(),
+                                    key=operator.itemgetter(1))[0][0])
 
 
 if __name__ == "__main__":
     # These checker is using only for your local testing
-    # It's run function in the same environment, but in the grading it will be in various
+    # It's run function in the same environment, but in the grading it will be
+    # in various
     from random import choice
     from re import sub
     from math import hypot
 
     def random_chicken(_, possible):
         return choice(possible)
-
 
     def distance_chicken(func):
         def run_chicken(yard, possible):
@@ -42,7 +149,6 @@ if __name__ == "__main__":
             return best
 
         return run_chicken
-
 
     CHICKEN_ALGORITHM = {
         "random": random_chicken,
@@ -88,7 +194,8 @@ if __name__ == "__main__":
                 return False
             chicken = find_position(yard, "C")
             possibles = find_free(yard, chicken)
-            chicken_action, new_chicken = CHICKEN_ALGORITHM[chicken_algorithm](yard, possibles)
+            chicken_action, new_chicken = CHICKEN_ALGORITHM[
+                chicken_algorithm](yard, possibles)
             positions = [find_position(yard, str(i + 1)) for i in range(N)]
             new_positions = []
             for i, (x, y) in enumerate(positions):
@@ -109,7 +216,8 @@ if __name__ == "__main__":
                 return True
 
             # update yard
-            temp_yard = [[ch if ch in ".X" else "." for ch in row] for row in yard]
+            temp_yard = [[ch if ch in ".X" else "." for ch in row]
+                         for row in yard]
             for i, (x, y) in enumerate(new_positions):
                 temp_yard[x][y] = str(i + 1)
             temp_yard[new_chicken[0]][new_chicken[1]] = "C"
@@ -117,18 +225,18 @@ if __name__ == "__main__":
         print(ERROR_TIRED)
         return False
 
-    assert checker(hunt, ("......",
-                          ".1.XX.",
-                          "...CX.",
-                          ".XX.X.",
-                          "...2..",
-                          "......"), "random"), "Example 1"
-    assert checker(hunt, ("......",
-                          ".1.XX.",
-                          "...CX.",
-                          ".XX.X.",
-                          "...2..",
-                          "......"), "run_away"), "Example 1"
+    # assert checker(hunt, ("......",
+    #                       ".1.XX.",
+    #                       "...CX.",
+    #                       ".XX.X.",
+    #                       "...2..",
+    #                       "......"), "random"), "Example 1"
+    # assert checker(hunt, ("......",
+    #                       ".1.XX.",
+    #                       "...CX.",
+    #                       ".XX.X.",
+    #                       "...2..",
+    #                       "......"), "run_away"), "Example 1"
     assert checker(hunt, ("......",
                           ".1.XX.",
                           "...CX.",
@@ -162,4 +270,3 @@ if __name__ == "__main__":
                           ".......",
                           ".......",
                           "...1...")), "Clear Random"
-
