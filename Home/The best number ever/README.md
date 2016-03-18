ba36fc2811a6f0e581263ce68593d90b356adea7
Ken M. Lai <ken.mercus.lai@gmail.com>
2016-03-19 10:54:42 +1100

    feat: the best number ever

---
+++ Home/The best number ever/README.md
@@ -0,0 +1,1 @@
+Mission URL: http://www.checkio.org/mission/the-best-number-ever/
--- Home/The best number ever/The best number ever.py
+++
@@ -1,6 +0,0 @@
-def checkio():
-    return 73  # if you are Sheldon
-
-if __name__ == '__main__':
-    # These "asserts" using only for self-checking and not necessary for auto-testing
-    assert isinstance(the_best_number(), (int, float, complex))
