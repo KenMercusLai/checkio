273b78210f834b4a468dfd6882a3b22bc8815ed0
Ken M. Lai <ken.mercus.lai@gmail.com>
2018-05-05 00:31:36 +1000

    feat(Elementary): Secret Messsage

---
+++ checkio/Elementary/Secret Message/secret_message.py
@@ -0,0 +1,14 @@
+import re
+
+
+def find_message(text):
+    return ''.join(re.findall('[A-Z]', text))
+
+
+if __name__ == '__main__':  # pragma: no cover
+    # These "asserts" using only for self-checking and not necessary for
+    # auto-testing
+    assert find_message(
+        u"How are you? Eh, ok. Low or Lower? Ohhh.") == "HELLO", "hello"
+    assert find_message(u"hello world!") == "", "Nothing"
+    assert find_message(u"HELLO WORLD!!!") == "HELLOWORLD", "Capitals"
