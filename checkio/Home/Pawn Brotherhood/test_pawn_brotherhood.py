828255bf78129ae535a1ede1833d90e90aa900b6
Ken M. Lai <ken.mercus.lai@gmail.com>
2018-05-04 22:04:41 +1000

    feat(Home): Pawn Brotherhood
    # |<----  Using a Maximum Of 50 Characters  ---->|
    
    
    # Explain why this change is being made
    # |<----   Try To Limit Each Line to a Maximum Of 72 Characters   ---->|
    
    # Provide links or keys to any relevant tickets, articles or other resources
    # Example: Github issue #23
    
    # --- COMMIT END ---
    # Type can be 
    #    feat (new feature)
    #    fix (bug fix)
    #    docs (changes to documentation)
    #    style (formatting, missing semi colons, etc; no code change)
    #    refactor (refactoring production code)
    #    test (adding missing tests, refactoring tests; no production code change)
    #    chore (updating grunt tasks etc; no production code change)
    # --------------------
    # Remember to
    #    Separate subject from body with a blank line
    #    Limit the subject line to 50 characters
    #    Capitalize the subject line
    #    Do not end the subject line with a period
    #    Use the imperative mood in the subject line
    #    Wrap the body at 72 characters
    #    Use the body to explain what and why vs. how
    #    Can use multiple lines with "-" for bullet points in body
    # --------------------
    # For more information about this template, check out
    # https://gist.github.com/adeekshith/cd4c95a064977cdc6c50

---
+++ checkio/Home/Pawn Brotherhood/test_pawn_brotherhood.py
@@ -0,0 +1,95 @@
+import unittest
+
+from pawn_brotherhood import safe_pawns
+
+
+class Tests(unittest.TestCase):
+    TESTS = {
+        "Basics": [
+            {
+                "input": ["b4", "d4", "f4", "c3", "e3", "g5", "d2"],
+                "answer": 6,
+                "explanation": ["d2"]
+            },
+            {
+                "input": ["b4", "c4", "d4", "e4", "f4", "g4", "e5"],
+                "answer": 1,
+                "explanation": ["b4", "c4", "d4", "e4", "f4", "g4"]
+            },
+        ],
+        "Edge": [
+            {
+                "input": ["e4"],
+                "answer": 0,
+                "explanation": ["e4"]
+            },
+            {
+                "input": ["e8"],
+                "answer": 0,
+                "explanation": ["e8"]
+            },
+            {
+                "input": ["a1", "b2", "c3", "d4", "e5", "f6", "g7", "h8"],
+                "answer": 7,
+                "explanation": ["a1"]
+            },
+            {
+                "input": ["a8", "b7", "c6", "d5", "e4", "f3", "g2", "h1"],
+                "answer": 7,
+                "explanation": ["h1"]
+            },
+            {
+                "input": ["a1", "b2", "c3", "d4", "e5", "f6", "g7", "h8"],
+                "answer": 7,
+                "explanation": ["a1"]
+            },
+            {
+                "input": ["a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"],
+                "answer": 0,
+                "explanation": ["a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"]
+            },
+            {
+                "input": ["a1", "a2", "a3", "a4", "h1", "h2", "h3", "h4"],
+                "answer": 0,
+                "explanation": ["a1", "a2", "a3", "a4", "h1", "h2", "h3", "h4"]
+            },
+        ],
+        "Extra": [
+            {
+                "input": ["b4", "c4", "d4", "e4", "f4", "g4", "e3"],
+                "answer": 2,
+                "explanation": ["b4", "c4", "e4", "g4", "e3"]
+            },
+            {
+                "input": ["e7", "e6", "d5", "f5", "c4", "e4", "g4", "e8"],
+                "answer": 3,
+                "explanation": ["e8", "e7", "c4", "e4", "g4"]
+            },
+            {
+                "input": ["a2", "b4", "c6", "d8", "e1", "f3", "g5", "h8"],
+                "answer": 0,
+                "explanation": ["a2", "b4", "c6", "d8", "e1", "f3", "g5", "h8"]
+            },
+            {
+                "input": ["b6", "a7", "b8", "c7", "g1", "f2", "h2", "g3"],
+                "answer": 6,
+                "explanation": ["b6", "g1"]
+            },
+        ]
+    }
+
+    def test_Basics(self):
+        for i in self.TESTS['Basics']:
+            assert safe_pawns(i['input']) == i['answer'], i['input']
+
+    def test_Edge(self):
+        for i in self.TESTS['Edge']:
+            assert safe_pawns(i['input']) == i['answer'], i['input']
+
+    def test_Extra(self):
+        for i in self.TESTS['Extra']:
+            assert safe_pawns(i['input']) == i['answer'], i['input']
+
+
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
