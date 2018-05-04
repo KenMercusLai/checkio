9e1e94477e66f0e982eae5d16744c185bc5a100c
Ken M. Lai <ken.mercus.lai@gmail.com>
2018-05-05 00:37:11 +1000

    feat(Elementary): Three Words
    # |<----  Using a Maximum Of 50 Characters  ---->|
    
    
    # Explain why this change is being made
    # |<----   Try To Limit Each Line to a Maximum Of 72 Characters   ---->|
    
    # Provide links or keys to any relevant tickets, articles or other resources
    # Example: Github issue #23
    
    # --- COMMIT END ---
    # Type can be 
    #    feat (new feature)
    #    fix (bug fix)
    #    docs (changes to documentation)
    #    style (formatting, missing semi colons, etc; no code change)
    #    refactor (refactoring production code)
    #    test (adding missing tests, refactoring tests; no production code change)
    #    chore (updating grunt tasks etc; no production code change)
    # --------------------
    # Remember to
    #    Separate subject from body with a blank line
    #    Limit the subject line to 50 characters
    #    Capitalize the subject line
    #    Do not end the subject line with a period
    #    Use the imperative mood in the subject line
    #    Wrap the body at 72 characters
    #    Use the body to explain what and why vs. how
    #    Can use multiple lines with "-" for bullet points in body
    # --------------------
    # For more information about this template, check out
    # https://gist.github.com/adeekshith/cd4c95a064977cdc6c50

---
+++ checkio/Elementary/Three Words/three_words.py
@@ -0,0 +1,20 @@
+def is_word(x):
+    return 1 if x.isalpha() else 0
+
+
+def checkio(words):
+    word_list = [is_word(i) for i in words.split()]
+    consecutive_words = [sum(word_list[i:i + 3])
+                         for i in range(len(word_list))
+                         if word_list[i]]
+    return any([True for i in consecutive_words if i >= 3])
+
+
+# These "asserts" using only for self-checking and not necessary for
+# auto-testing
+if __name__ == '__main__':  # pragma: no cover
+    assert checkio(u"Hello World hello") is True, "Hello"
+    assert checkio(u"He is 123 man") is False, "123 man"
+    assert checkio(u"1 2 3 4") is False, "Digits"
+    assert checkio(u"bla bla bla bla") is True, "Bla Bla"
+    assert checkio(u"Hi") is False, "Hi"
