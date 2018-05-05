40c2587bb1f682bc4032a41a934b21af3314e0b1
Ken M. Lai <ken.mercus.lai@gmail.com>
2018-05-05 15:30:56 +1000

    feat(O'Reilly): Friends
    # |<----  Using a Maximum Of 50 Characters  ---->|
    
    
    # Explain why this change is being made
    # |<----   Try To Limit Each Line to a Maximum Of 72 Characters   ---->|
    
    # Provide links or keys to any relevant tickets, articles or other resources
    # Example: Github issue #23
    
    # --- COMMIT END ---
    # Type can be 
    #    feat (new feature)
    #    fix (bug fix)
    #    docs (changes to documentation)
    #    style (formatting, missing semi colons, etc; no code change)
    #    refactor (refactoring production code)
    #    test (adding missing tests, refactoring tests; no production code change)
    #    chore (updating grunt tasks etc; no production code change)
    # --------------------
    # Remember to
    #    Separate subject from body with a blank line
    #    Limit the subject line to 50 characters
    #    Capitalize the subject line
    #    Do not end the subject line with a period
    #    Use the imperative mood in the subject line
    #    Wrap the body at 72 characters
    #    Use the body to explain what and why vs. how
    #    Can use multiple lines with "-" for bullet points in body
    # --------------------
    # For more information about this template, check out
    # https://gist.github.com/adeekshith/cd4c95a064977cdc6c50

---
+++ checkio/O'Reilly/Friends/test_friends.py
@@ -0,0 +1,69 @@
+import unittest
+from friends import Friends
+
+
+class Tests(unittest.TestCase):
+    def test_Init(self):
+        Friends(({"a", "b"}, {"b", "c"}, {"c", "a"}, {"a", "c"}))
+        Friends([{"1", "2"}, {"3", "1"}])
+
+    def test_Add(self):
+        f = Friends([{"1", "2"}, {"3", "1"}])
+        assert f.add({"2", "4"}) is True
+
+        f = Friends([{"And", "Or"}, {"For", "And"}])
+        assert f.add({"It", "Am"}) is True
+
+        f = Friends([{"And", "Or"}, {"For", "And"}])
+        assert f.add({"Or", "And"}) is False
+
+    def test_Remove(self):
+        f = Friends([{"1", "2"}, {"3", "1"}])
+        assert f.remove({"2", "4"}) is False
+
+        f = Friends([{"1", "2"}, {"3", "1"}])
+        assert f.remove({"11", "12"}) is False
+
+        f = Friends([{"And", "Or"}, {"For", "And"}])
+        assert f.remove({"And", "Or"}) is True
+
+    def test_Names(self):
+        f = Friends(({"nikola", "sophia"},
+                     {"stephen", "robot"},
+                     {"sophia", "pilot"}))
+        n = f.names()
+        assert n == {"nikola", "sophia", "robot", "pilot", "stephen"}
+
+        f = Friends(({"nikola", "sophia"},
+                     {"stephen", "robot"},
+                     {"sophia", "pilot"}))
+        f.remove({"stephen", "robot"})
+        n = f.names()
+        assert n == {"nikola", "sophia", "pilot"}
+
+    def test_Connected(self):
+        f = Friends(({"nikola", "sophia"},
+                     {"stephen", "robot"},
+                     {"sophia", "pilot"}))
+        n = f.connected("nikola")
+        assert n == {"sophia"}
+
+        f = Friends(({"nikola", "sophia"},
+                     {"stephen", "robot"},
+                     {"sophia", "pilot"}))
+        n = f.connected("sophia")
+        assert n == {"nikola", "pilot"}
+
+        f = Friends(({"nikola", "sophia"},
+                     {"stephen", "robot"},
+                     {"sophia", "pilot"}))
+        n = f.connected("DDD")
+        assert n == set()
+
+        f = Friends(({"nikola", "sophia"},
+                     {"stephen", "robot"},
+                     {"sophia", "pilot"}))
+        f.add({"sophia", "stephen"})
+        f.remove({"sophia", "nikola"})
+        n = f.connected("sophia")
+        assert n == {"stephen", "pilot"}
