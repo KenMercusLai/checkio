bca24dadd0dc39b3b5ed486d3e13e84f0e2f83eb
Ken M. Lai <ken.mercus.lai@gmail.com>
2018-05-05 14:22:06 +1000

    feat(O'Reilly): Feed Pigeons
    # |<----  Using a Maximum Of 50 Characters  ---->|
    
    
    # Explain why this change is being made
    # |<----   Try To Limit Each Line to a Maximum Of 72 Characters   ---->|
    
    # Provide links or keys to any relevant tickets, articles or other resources
    # Example: Github issue #23
    
    # --- COMMIT END ---
    # Type can be 
    #    feat (new feature)
    #    fix (bug fix)
    #    docs (changes to documentation)
    #    style (formatting, missing semi colons, etc; no code change)
    #    refactor (refactoring production code)
    #    test (adding missing tests, refactoring tests; no production code change)
    #    chore (updating grunt tasks etc; no production code change)
    # --------------------
    # Remember to
    #    Separate subject from body with a blank line
    #    Limit the subject line to 50 characters
    #    Capitalize the subject line
    #    Do not end the subject line with a period
    #    Use the imperative mood in the subject line
    #    Wrap the body at 72 characters
    #    Use the body to explain what and why vs. how
    #    Can use multiple lines with "-" for bullet points in body
    # --------------------
    # For more information about this template, check out
    # https://gist.github.com/adeekshith/cd4c95a064977cdc6c50

---
+++ checkio/O'Reilly/Feed Pigeons/feed_pigeons.py
@@ -0,0 +1,22 @@
+def checkio(number):
+    pigeon_queue = []
+    new_pigeon = 1
+    while number:
+        pigeon_queue += [0] * new_pigeon
+        for i in range(len(pigeon_queue)):
+            pigeon_queue[i] += 1
+            number -= 1
+            if number == 0:
+                break
+        new_pigeon += 1
+    return len(list(filter(lambda x: x, pigeon_queue)))
+
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for
+    # auto-testing
+    assert checkio(1) == 1, "1st example"
+    assert checkio(2) == 1, "2nd example"
+    assert checkio(5) == 3, "3rd example"
+    assert checkio(10) == 6, "4th example"
+    print(checkio(3))
