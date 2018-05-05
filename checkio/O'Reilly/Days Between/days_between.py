e596fc0ccdf994af864b689d50ff8a4a7f65ddca
Ken M. Lai <ken.mercus.lai@gmail.com>
2018-05-05 14:02:49 +1000

    feat(O'Reilly): Days Between
    # |<----  Using a Maximum Of 50 Characters  ---->|
    
    
    # Explain why this change is being made
    # |<----   Try To Limit Each Line to a Maximum Of 72 Characters   ---->|
    
    # Provide links or keys to any relevant tickets, articles or other resources
    # Example: Github issue #23
    
    # --- COMMIT END ---
    # Type can be 
    #    feat (new feature)
    #    fix (bug fix)
    #    docs (changes to documentation)
    #    style (formatting, missing semi colons, etc; no code change)
    #    refactor (refactoring production code)
    #    test (adding missing tests, refactoring tests; no production code change)
    #    chore (updating grunt tasks etc; no production code change)
    # --------------------
    # Remember to
    #    Separate subject from body with a blank line
    #    Limit the subject line to 50 characters
    #    Capitalize the subject line
    #    Do not end the subject line with a period
    #    Use the imperative mood in the subject line
    #    Wrap the body at 72 characters
    #    Use the body to explain what and why vs. how
    #    Can use multiple lines with "-" for bullet points in body
    # --------------------
    # For more information about this template, check out
    # https://gist.github.com/adeekshith/cd4c95a064977cdc6c50

---
+++ checkio/O'Reilly/Days Between/days_between.py
@@ -0,0 +1,16 @@
+import datetime
+
+
+def days_diff(date1, date2):
+    days = (datetime.datetime(*date2) - datetime.datetime(*date1)).days
+    if days < 0:
+        return -days
+    return days
+
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for
+    # auto-testing
+    assert days_diff((1982, 4, 19), (1982, 4, 22)) == 3
+    assert days_diff((2014, 1, 1), (2014, 8, 27)) == 238
+    assert days_diff((2014, 8, 27), (2014, 1, 1)) == 238
