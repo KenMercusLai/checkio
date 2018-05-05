6ac65625102834995a8d30b38e38c1e29f918938
Ken M. Lai <ken.mercus.lai@gmail.com>
2018-05-05 18:46:59 +1000

    feat(Dropbox): Break Rings
    # |<----  Using a Maximum Of 50 Characters  ---->|
    
    
    # Explain why this change is being made
    # |<----   Try To Limit Each Line to a Maximum Of 72 Characters   ---->|
    
    # Provide links or keys to any relevant tickets, articles or other resources
    # Example: Github issue #23
    
    # --- COMMIT END ---
    # Type can be 
    #    feat (new feature)
    #    fix (bug fix)
    #    docs (changes to documentation)
    #    style (formatting, missing semi colons, etc; no code change)
    #    refactor (refactoring production code)
    #    test (adding missing tests, refactoring tests; no production code change)
    #    chore (updating grunt tasks etc; no production code change)
    # --------------------
    # Remember to
    #    Separate subject from body with a blank line
    #    Limit the subject line to 50 characters
    #    Capitalize the subject line
    #    Do not end the subject line with a period
    #    Use the imperative mood in the subject line
    #    Wrap the body at 72 characters
    #    Use the body to explain what and why vs. how
    #    Can use multiple lines with "-" for bullet points in body
    # --------------------
    # For more information about this template, check out
    # https://gist.github.com/adeekshith/cd4c95a064977cdc6c50

---
+++ checkio/Dropbox/Break Rings/break_rings.py
@@ -0,0 +1,22 @@
+from itertools import combinations
+
+
+def break_rings(rings):
+    elements = set([j for i in rings for j in i])
+    for i in range(1, len(rings) - 1):
+        for j in combinations(elements, i):
+            if not [k for k in rings if not set(k).intersection(j)]:
+                return len(j)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    # These "asserts" using only for self-checking and not necessary for
+    # auto-testing
+    assert break_rings(({1, 2}, {2, 3}, {3, 4},
+                        {4, 5}, {5, 6}, {4, 6})) == 3, "example"
+    assert break_rings(({1, 2}, {1, 3}, {1, 4}, {2, 3},
+                        {2, 4}, {3, 4})) == 3, "All to all"
+    assert break_rings(({5, 6}, {4, 5}, {3, 4}, {3, 2},
+                        {2, 1}, {1, 6})) == 3, "Chain"
+    assert break_rings(({8, 9}, {1, 9}, {1, 2}, {2, 3}, {3, 4},
+                        {4, 5}, {5, 6}, {6, 7}, {8, 7})) == 5, "Long chain"
