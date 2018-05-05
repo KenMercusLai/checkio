427ec896204441446d2bd94ddd01541f3dd31c98
Ken M. Lai <ken.mercus.lai@gmail.com>
2018-05-05 13:53:44 +1000

    feat(Elementary): Date and Time Converter
    # |<----  Using a Maximum Of 50 Characters  ---->|
    
    
    # Explain why this change is being made
    # |<----   Try To Limit Each Line to a Maximum Of 72 Characters   ---->|
    
    # Provide links or keys to any relevant tickets, articles or other resources
    # Example: Github issue #23
    
    # --- COMMIT END ---
    # Type can be 
    #    feat (new feature)
    #    fix (bug fix)
    #    docs (changes to documentation)
    #    style (formatting, missing semi colons, etc; no code change)
    #    refactor (refactoring production code)
    #    test (adding missing tests, refactoring tests; no production code change)
    #    chore (updating grunt tasks etc; no production code change)
    # --------------------
    # Remember to
    #    Separate subject from body with a blank line
    #    Limit the subject line to 50 characters
    #    Capitalize the subject line
    #    Do not end the subject line with a period
    #    Use the imperative mood in the subject line
    #    Wrap the body at 72 characters
    #    Use the body to explain what and why vs. how
    #    Can use multiple lines with "-" for bullet points in body
    # --------------------
    # For more information about this template, check out
    # https://gist.github.com/adeekshith/cd4c95a064977cdc6c50

---
+++ checkio/Elementary/Date and Time Converter/date_time_converter.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+
+def date_time(time: str) -> str:
+    converted_dt = datetime.strptime(time, "%d.%m.%Y %H:%M")
+    preformated_date = converted_dt.strftime('{} %B %Y year')
+    preformated_date = preformated_date.format(converted_dt.day)
+    preformated_hour = '{} hour'.format(converted_dt.hour)
+    if converted_dt.hour != 1:
+        preformated_hour += 's'
+    preformated_minute = '{} minute'.format(converted_dt.minute)
+    if converted_dt.hour != 1:
+        preformated_minute += 's'
+    return ' '.join([preformated_date, preformated_hour, preformated_minute])
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(date_time('01.01.2000 00:00'))
+
+    # These "asserts" using only for self-checking
+    # and not necessary for auto-testing
+    assert date_time("01.01.2000 00:00") == "1 January 2000 year 0 hours 0 minutes", "Millenium"
+    assert date_time("09.05.1945 06:30") == "9 May 1945 year 6 hours 30 minutes", "Victory"
+    assert date_time("20.11.1990 03:55") == "20 November 1990 year 3 hours 55 minutes", "Somebody was born"
+    print("Coding complete? Click 'Check' to earn cool rewards!")
---
+++ checkio/Elementary/Date and Time Converter/test_date_time_converter.py
@@ -0,0 +1,44 @@
+import unittest
+
+from date_time_converter import date_time
+
+
+class Tests(unittest.TestCase):
+    TESTS = {
+        "Basics": [
+            {
+                "input": '01.01.2000 00:00',
+                "answer": "1 January 2000 year 0 hours 0 minutes"
+            },
+            {
+                "input": '09.05.1945 06:30',
+                "answer": "9 May 1945 year 6 hours 30 minutes"
+            }
+        ],
+        "Extra": [
+            {
+                "input": '20.11.1990 03:55',
+                "answer": "20 November 1990 year 3 hours 55 minutes"
+            },
+            {
+                "input": '09.07.1995 16:50',
+                "answer": "9 July 1995 year 16 hours 50 minutes"
+            },
+            {
+                "input": '11.04.1812 01:01',
+                "answer": "11 April 1812 year 1 hour 1 minute"
+            }
+        ]
+    }
+
+    def test_Basics(self):
+        for i in self.TESTS['Basics']:
+            assert date_time(i['input']) == i['answer']
+
+    def test_Extra(self):
+        for i in self.TESTS['Extra']:
+            assert date_time(i['input']) == i['answer']
+
+
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
