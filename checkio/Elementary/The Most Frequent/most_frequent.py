eeed97c1787ef625c7ce5ae994253462854472f5
Ken M. Lai <ken.mercus.lai@gmail.com>
2018-05-05 13:25:52 +1000

    feat(Elementary): The Most Frequent
    # |<----  Using a Maximum Of 50 Characters  ---->|
    
    
    # Explain why this change is being made
    # |<----   Try To Limit Each Line to a Maximum Of 72 Characters   ---->|
    
    # Provide links or keys to any relevant tickets, articles or other resources
    # Example: Github issue #23
    
    # --- COMMIT END ---
    # Type can be 
    #    feat (new feature)
    #    fix (bug fix)
    #    docs (changes to documentation)
    #    style (formatting, missing semi colons, etc; no code change)
    #    refactor (refactoring production code)
    #    test (adding missing tests, refactoring tests; no production code change)
    #    chore (updating grunt tasks etc; no production code change)
    # --------------------
    # Remember to
    #    Separate subject from body with a blank line
    #    Limit the subject line to 50 characters
    #    Capitalize the subject line
    #    Do not end the subject line with a period
    #    Use the imperative mood in the subject line
    #    Wrap the body at 72 characters
    #    Use the body to explain what and why vs. how
    #    Can use multiple lines with "-" for bullet points in body
    # --------------------
    # For more information about this template, check out
    # https://gist.github.com/adeekshith/cd4c95a064977cdc6c50

---
+++ checkio/Elementary/The Most Frequent/most_frequent.py
@@ -0,0 +1,25 @@
+from collections import Counter
+
+
+def most_frequent(data):
+    return Counter(data).most_common(1)[0][0]
+
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking
+    # and not necessary for auto-testing
+    print('Example:')
+    print(most_frequent([
+        'a', 'b', 'c',
+        'a', 'b',
+        'a'
+    ]))
+
+    assert most_frequent([
+        'a', 'b', 'c',
+        'a', 'b',
+        'a'
+    ]) == 'a'
+
+    assert most_frequent(['a', 'a', 'bi', 'bi', 'bi']) == 'bi'
+    print('Done')
---
+++ checkio/Elementary/The Most Frequent/test_most_frequent.py
@@ -0,0 +1,48 @@
+import unittest
+
+from most_frequent import most_frequent
+
+
+class Tests(unittest.TestCase):
+    TESTS = {
+        "Basics": [
+            {
+                "input": [
+                    'a', 'b', 'c',
+                    'a', 'b',
+                    'a'
+                ],
+                "answer": 'a'
+            },
+            {
+                "input": ['a', 'a', 'bi', 'bi', 'bi'],
+                "answer": 'bi'
+            }
+        ],
+        "Extra": [
+            {
+                "input": ['a'],
+                "answer": 'a'
+            },
+            {
+                "input": ['a', 'a'],
+                "answer": 'a'
+            },
+            {
+                "input": ['a', 'a', 'z'],
+                "answer": 'a'
+            }
+        ]
+    }
+
+    def test_Basics(self):
+        for i in self.TESTS['Basics']:
+            assert most_frequent(i['input']) == i['answer']
+
+    def test_Extra(self):
+        for i in self.TESTS['Extra']:
+            assert most_frequent(i['input']) == i['answer']
+
+
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
