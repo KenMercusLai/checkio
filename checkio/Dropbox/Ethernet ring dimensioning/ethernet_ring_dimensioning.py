503b2edba573bd7697395bd3c8babbaf2d57008c
Ken M. Lai <ken.mercus.lai@gmail.com>
2018-06-30 20:55:32 +0800

    feat(Dropbox): Ethernet ring dimensioning
    # |<----  Using a Maximum Of 50 Characters  ---->|
    
    
    # Explain why this change is being made
    # |<----   Try To Limit Each Line to a Maximum Of 72 Characters   ---->|
    
    # Provide links or keys to any relevant tickets, articles or other resources
    # Example: Github issue #23
    
    # --- COMMIT END ---
    # Type can be 
    #    feat     (new feature)
    #    fix      (bug fix)
    #    refactor (refactoring production code)
    #    style    (formatting, missing semi colons, etc; no code change)
    #    docs     (changes to documentation)
    #    test     (adding or refactoring tests; no production code change)
    #    chore    (updating grunt tasks etc; no production code change)
    # --------------------
    # Remember to
    #    Capitalize the subject line
    #    Use the imperative mood in the subject line
    #    Do not end the subject line with a period
    #    Separate subject from body with a blank line
    #    Use the body to explain what and why vs. how
    #    Can use multiple lines with "-" for bullet points in body
    # --------------------
    # For more information about this template, check out
    # https://gist.github.com/adeekshith/cd4c95a064977cdc6c50

---
+++ checkio/Dropbox/Ethernet ring dimensioning/ethernet_ring_dimensioning.py
@@ -0,0 +1,72 @@
+from math import ceil
+ETHERNET = (100, 40, 10, 1, 0.1)  # Ethernet bandwidth capacity in Gbps
+
+
+def generate_path(ring, start, end):
+    # generate the shortest path from start to end.
+
+    # according to the task, the preference path in order is:
+    # 1. from start to end following the ring order if it's the shortest
+    # 2. from start to end following in the reversed ring order
+    # if it's the shortest
+    # 3. from end to start following in the ring order if it's the shortest
+
+    start_index = ring.index(start)
+    end_index = ring.index(end)
+
+    ring_chain = ring + ring
+    if start_index < end_index:
+        path = ring[start_index:end_index+1]
+        if len(path) * 2 > len(ring) + 2:
+            path = ring_chain[end_index:start_index+len(ring)+1]
+            path = path[::-1]
+    else:
+        path = ring[end_index:start_index+1]
+        if len(path) * 2 == len(ring) + 2:
+            path = ring_chain[start_index:end_index+len(ring)+1]
+        elif len(path) * 2 > len(ring) + 2:
+            path = ring_chain[start_index:end_index+len(ring)+1]
+            path = path[::-1]
+    return path
+
+
+def segment_bandwidth(ring, flows):
+    ring_segments = (list(map(lambda x: x[0]+x[1], zip(ring, ring[1:])))
+                     + [ring[-1]+ring[0]])
+    path_segments = {i: 0 for i in ring_segments}
+    for i in flows:
+        start, end, bandwidth = i[0][0], i[0][1], i[1]
+        path = generate_path(ring, start, end)
+        shortest_path_segments = map(lambda x: x[0]+x[1], zip(path, path[1:]))
+        for j in shortest_path_segments:
+            try:
+                path_segments[j] += bandwidth
+            except KeyError:
+                path_segments[j[::-1]] += bandwidth
+    return path_segments
+
+
+def checkio(ring, *flows):
+    result = [0, 0, 0, 0, 0]
+    for i in sorted(segment_bandwidth(ring, flows).values(), reverse=True):
+        if i > 40:
+            result[0] += ceil(i / 100)
+        elif i > 10:
+            result[1] += 1
+        elif i > 1:
+            result[2] += 1
+        elif i > 0.1:
+            result[3] += 1
+        elif i > 0:
+            result[4] += 1
+    return result
+
+
+if __name__ == '__main__':  # pragma: no cover
+    # These "asserts" are used only for self-checking and not necessary for
+    # auto-testing
+    assert checkio("AEFCBG",
+                   ("AC", 5), ("EC", 10), ("AB", 60)) == [2, 2, 1, 0, 0]
+    assert checkio("ABCDEFGH", ("AD", 4)) == [0, 0, 3, 0, 0]
+    assert checkio("ABCDEFGH", ("AD", 4), ("EA", 41)) == [4, 0, 3, 0, 0]
+    assert checkio("ABCDEFGH", ["AD", 4], ["DA", 4]) == [0, 0, 3, 0, 0]
---
+++ checkio/Dropbox/Ethernet ring dimensioning/test_ethernet_ring_dimensioning.py
@@ -0,0 +1,67 @@
+import unittest
+
+from ethernet_ring_dimensioning import checkio, generate_path, segment_bandwidth
+
+
+def test_generate_path():
+    assert generate_path('AEFCBG', 'A', 'B') == 'AGB'
+    assert generate_path('AEFCBG', 'B', 'A') == 'AGB'
+    assert generate_path('AEFCBG', 'C', 'A') == 'CBGA'
+    assert generate_path('AEFCBG', 'A', 'C') == 'AEFC'
+    assert generate_path('AEFCBG', 'E', 'C') == 'EFC'
+
+
+def test_segment_bandwidth():
+    assert segment_bandwidth('AEFCBG', 
+                             [("AC", 5), ("EC", 10), ("AB", 60)]) == {'AE': 5, 
+                                                                      'EF': 15, 
+                                                                      'FC': 15, 
+                                                                      'CB': 0, 
+                                                                      'BG': 60, 
+                                                                      'GA': 60}
+
+
+class Tests(unittest.TestCase):
+    TESTS = {
+        "Basics": [
+            {
+                "input": ["AEFCBG", ("AC", 5), ("EC", 10), ("AB", 60)],
+                "answer": [2, 2, 1, 0, 0]
+            },
+            {
+                "input": ["ABCDEFGH", ("AD", 4)],
+                "answer": [0, 0, 3, 0, 0]
+            },
+            {
+                "input": ["ABCDEFGH", ("AD", 4), ("EA", 41)],
+                "answer": [4, 0, 3, 0, 0]
+            }
+        ],
+        "Extra": [
+            {
+                "input": ["ADBC", ["AD", 1], ["DB", 9],
+                          ["BC", 0.345], ["CA", 13]],
+                "answer": [0, 1, 1, 2, 0]
+            },
+            {
+                "input": ["ABCDEFGH", ["AD", 4], ["DA", 4]],
+                "answer": [0, 0, 3, 0, 0]
+            },
+            {
+                "input": ["ABCDEFGH", ["AE", 170], ["EA", 10000], ["HF", 1]],
+                "answer": [410, 0, 0, 0, 0]
+            }
+        ]
+    }
+
+    def test_Basics(self):
+        for i in self.TESTS['Basics']:
+            assert checkio(i['input'][0], *i['input'][1:]) == i['answer']
+
+    def test_Extra(self):
+        for i in self.TESTS['Extra']:
+            assert checkio(i['input'][0], *i['input'][1:]) == i['answer']
+
+
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
