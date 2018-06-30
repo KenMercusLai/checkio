aad6c7edbb52a2c8b466f570abcbc2179c47fbb7
Ken M. Lai <ken.mercus.lai@gmail.com>
2018-06-30 21:25:46 +0800

    test: disabled pyflake on test import

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,3 @@
 """Integration tests configuration file."""
 
-from checkio.tests.conftest import pytest_configure  # pylint: disable=unused-import
+from checkio.tests.conftest import pytest_configure # noqa # pylint: disable=unused-import
