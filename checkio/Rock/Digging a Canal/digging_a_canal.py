8eae8e4cc520bc177723f1e0091e7cdb7381fffa
Ken M. Lai <ken.mercus.lai@gmail.com>
2019-02-02 13:20:59 +0800

    feat(Rock): Digging a Canal
    # |<----  Using a Maximum Of 50 Characters  ---->|
    
    # Explain why this change is being made
    # |<----   Try To Limit Each Line to a Maximum Of 72 Characters   ---->|
    
    # Provide links or keys to any relevant tickets, articles or other resources
    # Example: Github issue #23
    
    # --- COMMIT END ---
    # Type can be
    #    feat     (new feature)
    #    fix      (bug fix)
    #    refactor (refactoring production code)
    #    style    (formatting, missing semi colons, etc; no code change)
    #    docs     (changes to documentation)
    #    test     (adding or refactoring tests; no production code change)
    #    chore    (updating grunt tasks etc; no production code change)
    # --------------------
    # Remember to
    #    Capitalize the subject line
    #    Use the imperative mood in the subject line
    #    Do not end the subject line with a period
    #    Separate subject from body with a blank line
    #    Use the body to explain what and why vs. how
    #    Can use multiple lines with "-" for bullet points in body
    # --------------------
    # For more information about this template, check out
    # https://gist.github.com/adeekshith/cd4c95a064977cdc6c50

---
+++ checkio/Rock/Digging a Canal/digging_a_canal.py
@@ -0,0 +1,58 @@
+from copy import deepcopy
+
+
+def checkio(land_map):
+    adjencencies = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    land_map = [[0] * len(land_map[0])] + land_map
+    distance_map = deepcopy(land_map)
+    for i in range(1, len(distance_map)):
+        for j in range(len(distance_map[0])):
+            distance_map[i][j] = len(land_map) ** 2
+    while True:
+        changed = False
+        for row in range(1, len(land_map)):
+            for col in range(len(land_map[0])):
+                temp_distance = []
+                for i in adjencencies:
+                    if (0 <= row + i[0] <= len(land_map) - 1
+                            and 0 <= col + i[1] <= len(land_map[0]) - 1):
+                        temp_distance.append(
+                            land_map[row][col]
+                            + distance_map[row + i[0]][col + i[1]])
+                new_distance = min(temp_distance)
+                if new_distance != distance_map[row][col]:
+                    distance_map[row][col] = new_distance
+                    changed = True
+                    break
+            if changed:
+                break
+        if not changed:
+            break
+    return min(distance_map[-1])
+
+
+# These "asserts" using only for self-checking and not necessary for
+# auto-testing
+if __name__ == '__main__':
+    assert checkio([[1, 1, 1, 1, 0, 1, 1],
+                    [1, 1, 1, 1, 0, 0, 1],
+                    [1, 1, 1, 1, 1, 0, 1],
+                    [1, 1, 0, 1, 1, 0, 1],
+                    [1, 1, 0, 1, 1, 1, 1],
+                    [1, 0, 0, 1, 1, 1, 1],
+                    [1, 0, 1, 1, 1, 1, 1]]) == 2, "1st example"
+    assert checkio([[0, 0, 0, 0, 0, 0, 0],
+                    [1, 1, 1, 1, 1, 1, 1],
+                    [1, 1, 1, 1, 1, 1, 1],
+                    [1, 1, 1, 1, 1, 1, 1],
+                    [1, 1, 0, 1, 0, 1, 1],
+                    [1, 0, 0, 0, 0, 0, 1],
+                    [0, 0, 0, 0, 0, 0, 0]]) == 3, "2nd example"
+    assert checkio([[1, 1, 1, 1, 1, 0, 1, 1],
+                    [1, 0, 1, 1, 1, 0, 1, 1],
+                    [1, 0, 1, 0, 1, 0, 1, 0],
+                    [1, 0, 1, 1, 1, 0, 1, 1],
+                    [0, 0, 1, 1, 0, 0, 0, 0],
+                    [1, 0, 1, 1, 1, 1, 1, 1],
+                    [1, 0, 1, 1, 1, 1, 1, 1],
+                    [1, 1, 1, 0, 1, 1, 1, 1]]) == 2, "3rd example"
