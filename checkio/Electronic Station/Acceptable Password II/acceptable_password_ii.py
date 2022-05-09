2b7f69b4f1e8752cfeed545222c67364fac1f25f
Ken M. Lai <ken.mercus.lai@gmail.com>
2022-05-09 18:03:34 +1000

    add ELECTRONIC STATION/Acceptable Password II

---
+++ checkio/Electronic Station/Acceptable Password II/acceptable_password_ii.py
@@ -0,0 +1,15 @@
+from re import search
+
+
+def is_acceptable_password(password: str) -> bool:
+    return len(password) > 6 and bool(search(r'\d', password))
+
+
+if __name__ == "__main__":
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert is_acceptable_password("short") == False
+    assert is_acceptable_password("muchlonger") == False
+    assert is_acceptable_password("ashort") == False
+    assert is_acceptable_password("muchlonger5") == True
+    assert is_acceptable_password("sh5") == False
+    print("Coding complete? Click 'Check' to earn cool rewards!")
