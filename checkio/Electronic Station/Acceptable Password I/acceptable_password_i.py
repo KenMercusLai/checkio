a11d4efe64ab8946020fd97cf05fdaa72c1c4ff5
Ken M. Lai <ken.mercus.lai@gmail.com>
2022-05-09 17:58:55 +1000

    update Home/Acceptable Password I

---
+++ checkio/Electronic Station/Acceptable Password I/acceptable_password_i.py
@@ -0,0 +1,13 @@
+def is_acceptable_password(password: str) -> bool:
+    return len(password) > 6
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(is_acceptable_password('short'))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert is_acceptable_password('short') == False
+    assert is_acceptable_password('muchlonger') == True
+    assert is_acceptable_password('ashort') == False
+    print("Coding complete? Click 'Check' to earn cool rewards!")
