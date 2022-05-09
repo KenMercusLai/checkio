791de790a94697bfaf252091f99b01e0a09b7f68
Ken M. Lai <ken.mercus.lai@gmail.com>
2022-05-09 17:22:55 +1000

    update Home/Split List

---
+++ checkio/Home/Split List/split_list.py
@@ -0,0 +1,19 @@
+def split_list(items: list) -> list:
+    length = len(items)
+    if length % 2:
+        return [items[:length // 2 + 1], items[length // 2 + 1:]]
+    else:
+        return [items[:length // 2], items[length // 2:]]
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(split_list([1, 2, 3, 4, 5, 6]))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert split_list([1, 2, 3, 4, 5, 6]) == [[1, 2, 3], [4, 5, 6]]
+    assert split_list([1, 2, 3]) == [[1, 2], [3]]
+    assert split_list([1, 2, 3, 4, 5]) == [[1, 2, 3], [4, 5]]
+    assert split_list([1]) == [[1], []]
+    assert split_list([]) == [[], []]
+    print("Coding complete? Click 'Check' to earn cool rewards!")
