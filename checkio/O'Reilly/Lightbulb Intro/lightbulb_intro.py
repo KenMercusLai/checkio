b7f4a8ffe3da9d4d6714e2606b3108af7e86248e
Ken M. Lai <ken.mercus.lai@gmail.com>
2022-05-11 11:55:22 +1000

    O'REILLY/Lightbulb Intro

---
+++ checkio/O'Reilly/Lightbulb Intro/lightbulb_intro.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+from operator import sub
+from typing import List
+
+
+def chunk(lst, n):
+    return zip(*[iter(lst)] * n)
+
+
+def sum_light(els: List[datetime]) -> int:
+    return sum(map(lambda x: x.total_seconds(), list(map(lambda x: x[1]-x[0], chunk(els, 2)))))
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(sum_light([
+        datetime(2015, 1, 12, 10, 0, 0),
+        datetime(2015, 1, 12, 10, 10, 10),
+        datetime(2015, 1, 12, 11, 0, 0),
+        datetime(2015, 1, 12, 11, 10, 10),
+    ]))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert sum_light(els=[
+        datetime(2015, 1, 12, 10, 0, 0),
+        datetime(2015, 1, 12, 10, 10, 10),
+    ]) == 610
+
+    assert sum_light([
+        datetime(2015, 1, 12, 10, 0, 0),
+        datetime(2015, 1, 12, 10, 10, 10),
+        datetime(2015, 1, 12, 11, 0, 0),
+        datetime(2015, 1, 12, 11, 10, 10),
+    ]) == 1220
+
+    assert sum_light([
+        datetime(2015, 1, 12, 10, 0, 0),
+        datetime(2015, 1, 12, 10, 10, 10),
+        datetime(2015, 1, 12, 11, 0, 0),
+        datetime(2015, 1, 12, 11, 10, 10),
+        datetime(2015, 1, 12, 11, 10, 10),
+        datetime(2015, 1, 12, 12, 10, 10),
+    ]) == 4820
+
+    assert sum_light([
+        datetime(2015, 1, 12, 10, 0, 0),
+        datetime(2015, 1, 12, 10, 0, 1),
+    ]) == 1
+
+    assert sum_light([
+        datetime(2015, 1, 12, 10, 0, 0),
+        datetime(2015, 1, 12, 10, 0, 10),
+        datetime(2015, 1, 12, 11, 0, 0),
+        datetime(2015, 1, 13, 11, 0, 0),
+    ]) == 86410
+
+    print("The first mission in series is completed? Click 'Check' to earn cool rewards!")
