e8c73d59c3977c3a9630c287156cdc0f4c0dcf36
Ken M. Lai <ken.mercus.lai@gmail.com>
2022-05-11 18:54:28 +1000

    INITIATION/Multiply (Intro)

---
+++ checkio/Initiation/Multiply (Intro)/multiply_intro.py
@@ -0,0 +1,12 @@
+def mult_two(a, b):
+    return a * b
+
+
+if __name__ == "__main__":
+    print("Example:")
+    print(mult_two(3, 2))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert mult_two(3, 2) == 6
+    assert mult_two(1, 0) == 0
+    print("Coding complete? Click 'Check' to earn cool rewards!")
